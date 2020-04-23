25f1bc7d568b1e08990972da75034b071e8b16e7
Quyết Định <decisionnguyen@gmail.com>
2020-04-23 22:40:42 +0700

    toolbar

---
+++ index.js
@@ -0,0 +1,9 @@
+import RichTextEditor from './src/RichTextEditor';
+import RichTextToolbar from './src/RichTextToolbar';
+import {actions} from './src/const';
+
+export {
+  RichTextEditor,
+  RichTextToolbar,
+  actions,
+};
